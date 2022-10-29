e9130ad3c8caf9fa430a30d5db7eef9ecce9786b
Nikhil Pathak <nikhilpathak926@gmail.com>
2022-10-29 17:41:22 +0530

    Database setup

---
+++ database/db.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+func OpenDbConnection() *sql.DB {
+	db, err := sql.Open("postgres", "postgresql://postgres:postgres@user-mgmt-db:5433/user-mgmt-db?sslmode=disable")
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Unable to reach database: %v", err)
+	}
+
+	return db
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pathaknv/user-mgmt-grpc
 
 go 1.19
+
+require github.com/lib/pq v1.10.7
